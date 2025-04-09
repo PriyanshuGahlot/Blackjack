77103f60d649caa0bb23f002dc055f947a7f72ad
PriyanshuGahlot <anshu2084gahlot@gmail.com>
2025-04-10 00:15:30 +0530

    deck shuffleing implimented

--- index.js
+++ index.js
@@ -1,22 +1,36 @@
-function flipCard() {
-    const card = document.querySelector('.card');
-    card.classList.toggle('flip');
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
 
 class Deck {
     constructor() {
         this.cards = [];
         const vals = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
         const syms = ["♠", "♦", "♣", "♥"];
-        const container = document.getElementById("card-area");
 
         for (const sym of syms) {
             for (const val of vals) {
-                const card = createCardElement(val, sym);
-                container.appendChild(card);
-                this.cards.push(new Card(val, sym));
+                const card = new Card(val, sym)
+                this.cards.push(card);
             }
+        }
+    }
+
+    shuffle() {
+        let shuffledDeck = []
+        for(let i =0;i<52; i++){
+            let rand = getRandomInt(0,this.cards.length-1);
+            shuffledDeck[i] = this.cards[rand];
+            this.cards.splice(rand, 1);
+        }
+        this.cards = shuffledDeck;
+    }
+
+    display(){
+        const container = document.getElementById("card-area");
+        for(const card of this.cards){
+            const cardElement = createCardElement(card);
+            container.appendChild(cardElement);
         }
     }
 }
@@ -29,7 +43,9 @@ class Card{
     }
 }
 
-function createCardElement(value, symbol) {
+function createCardElement(cardObj) {
+    const value = cardObj.val
+    const symbol = cardObj.sym
     const card = document.createElement('div');
     card.className = 'card';
 
@@ -60,11 +76,16 @@ function createCardElement(value, symbol) {
 
     const container = document.createElement('div');
     container.className = 'card-container';
+    // container.classList.add('stack');
     container.appendChild(card);
+    // card.classList.toggle('flip');
 
     return container;
 }
 
 window.onload = () => {
     const deck = new Deck();
+    deck.shuffle()
+    deck.display()
 };
+
